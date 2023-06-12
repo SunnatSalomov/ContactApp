dbd03f06398b1118fa5df3584b79c8c7820652bd
sunnat <sunish2412@gmail.com>
2023-06-12 19:40:43 +0500

    3- process. there are Service, codes.

--- src/main/java/org/example/backend/model/Contact.java
+++ src/main/java/org/example/backend/model/Contact.java
@@ -1,7 +1,49 @@
 package org.example.backend.model;
 
+import java.util.UUID;
+
 public class Contact {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+
+    private final String  id;
+
+    private String firstName;
+
+    private String phoneNumber;
+
+    public Contact( String firstName, String phoneNumber) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 }
